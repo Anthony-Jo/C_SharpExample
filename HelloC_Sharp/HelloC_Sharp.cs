c988b7eac1894bd43a6120fb30552ec968da3a79
AnthonyJo <joseongkwang@gmail.com>
2021-01-01 22:04:10 +0900

    project init

---
+++ HelloC_Sharp/HelloC_Sharp.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloC_Sharp
+{  
+  class Program
+  {
+    static void main(string[] args)
+    {
+      int a = 100;
+      int b = 200;
+
+      Console.WriteLine("hello");
+      Console.WriteLine("a = " + a);
+      Console.WriteLine("a + b =" + (a + b));
+    }
+  }    
+}
+
+//Program.cs 가 사실상 컴파일 된다?
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_SharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+
+            Console.WriteLine("hello");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("a + b =" + (a + b));
+        }
+    }
+}
